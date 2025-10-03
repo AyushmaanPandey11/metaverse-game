204029adfa42793653d584f9f9f3f0830b108fc9
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2025-10-04 02:56:58 +0530

    frontend bootstrapped

--- application/apps/wss/src/Classes/User.ts
+++ application/apps/wss/src/Classes/User.ts
@@ -25,148 +25,149 @@ export class User {
     this.ws.on("message", async (data) => {
       const parsedData: wsMessageType = JSON.parse(data.toString());
       const roomInstance = Room.getInstance();
-      switch (parsedData.type) {
-        case "join":
-          const { spaceId, token } = parsedData.payload;
-          const decoded = jwt.verify(token, secretKey) as JwtPayload;
-          const userId = decoded.userId;
-          if (!userId) {
-            this.ws.close();
-            return;
-          }
-          this.userId = userId;
-          this.spaceId = spaceId;
-          const userSpace = await roomInstance.getSpaceData(this.spaceId);
-          if (!userSpace) {
-            this.ws.close();
-            return;
-          }
-          const roomUsers = Room.getInstance().spaces.get(spaceId) ?? [];
-          const userCoords = roomUsers.map((user) => ({
-            x: user.x,
-            y: user.y,
-          }));
-          const elementsCoords = userSpace.elements.map((ele) => ({
-            x: ele.x,
-            y: ele.y,
-          }));
-          const spawnPoint = this.findNewUserCoordinates(
-            userSpace.width,
-            userSpace.height,
-            userSpace.elements,
-            userCoords
-          );
-          if (!spawnPoint) {
-            this.ws.close();
-            return;
-          } else {
-            this.x = spawnPoint.x;
-            this.y = spawnPoint.y;
-          }
-          roomInstance.addUser(this, spaceId);
-          this.ws.send(
-            JSON.stringify({
-              type: "space-joined",
-              payload: {
-                spawn: {
-                  x: this.x,
-                  y: this.y,
-                },
-                users:
-                  roomInstance.spaces
-                    .get(spaceId)
-                    ?.filter((u) => u.id !== this.id)
-                    ?.map((u) => ({ id: u.userId! })) || [],
-              },
-            })
-          );
-          roomInstance.publish(
-            {
-              type: "user-join",
-              payload: {
-                userId: this.userId!,
-                x: this.x,
-                y: this.y,
-              },
-            },
-            this.id,
-            this.spaceId
-          );
-          break;
-
-        case "move":
-          if (!this.spaceId) return;
-          const { x, y } = parsedData.payload;
-          const xDisplacement = Math.abs(this.x - x);
-          const yDisplacement = Math.abs(this.y - y);
-          if (
-            (xDisplacement == 1 && yDisplacement == 0) ||
-            (xDisplacement == 0 && yDisplacement == 1)
-          ) {
+      try {
+        switch (parsedData.type) {
+          case "join":
+            const { spaceId, token } = parsedData.payload;
+            const decoded = jwt.verify(token, secretKey) as JwtPayload;
+            const userId = decoded.userId;
+            if (!userId) {
+              this.ws.close();
+              return;
+            }
+            this.userId = userId;
+            this.spaceId = spaceId;
             const userSpace = await roomInstance.getSpaceData(this.spaceId);
             if (!userSpace) {
               this.ws.close();
               return;
             }
-            // should be within the space bounds
-            if (
-              x <= userSpace.width &&
-              x >= 0 &&
-              y >= 0 &&
-              y <= userSpace.height!
-            ) {
-              const spaceUsers = roomInstance.spaces.get(this.spaceId!);
-              const occupiedCoords = new Set<string>();
-              userSpace.occupiedStaticTiles.forEach((coord) =>
-                occupiedCoords.add(coord)
-              );
-              spaceUsers?.forEach((u) => {
-                if (u.id !== this.id) {
-                  occupiedCoords.add(`${u.x},${u.y}`);
-                }
-              });
-              if (!occupiedCoords.has(`${x},${y}`)) {
-                this.x = x;
-                this.y = y;
-                roomInstance.publish(
-                  {
-                    type: "movement",
-                    payload: {
-                      x: this.x,
-                      y: this.y,
-                      userId: this.userId!,
-                    },
-                  },
-                  this.id,
-                  this.spaceId!
-                );
-              } else {
-                this.ws.send(
-                  JSON.stringify({
-                    type: "movement-rejected",
-                    payload: {
-                      x: this.x,
-                      y: this.y,
-                    },
-                  })
-                );
-              }
+            const roomUsers = Room.getInstance().spaces.get(spaceId) ?? [];
+            const userCoords = roomUsers.map((user) => ({
+              x: user.x,
+              y: user.y,
+            }));
+            const spawnPoint = this.findNewUserCoordinates(
+              userSpace.width,
+              userSpace.height,
+              userSpace.elements,
+              userCoords
+            );
+            if (!spawnPoint) {
+              this.ws.close();
+              return;
+            } else {
+              this.x = spawnPoint.x;
+              this.y = spawnPoint.y;
             }
-          } else {
-            // rejected case, can only take one step either of the axis
+            roomInstance.addUser(this, spaceId);
             this.ws.send(
               JSON.stringify({
-                type: "movement-rejected",
+                type: "space-joined",
                 payload: {
-                  x: this.x,
-                  y: this.y,
+                  spawn: {
+                    x: this.x,
+                    y: this.y,
+                  },
+                  users:
+                    roomInstance.spaces
+                      .get(spaceId)
+                      ?.filter((u) => u.id !== this.id)
+                      ?.map((u) => ({ id: u.userId! })) || [],
                 },
               })
             );
-          }
-          break;
-        default:
-          break;
+            roomInstance.publish(
+              {
+                type: "user-join",
+                payload: {
+                  userId: this.userId!,
+                  x: this.x,
+                  y: this.y,
+                },
+              },
+              this.id,
+              this.spaceId
+            );
+            break;
+
+          case "move":
+            if (!this.spaceId) return;
+            const { x, y } = parsedData.payload;
+            const xDisplacement = Math.abs(this.x - x);
+            const yDisplacement = Math.abs(this.y - y);
+            if (
+              (xDisplacement == 1 && yDisplacement == 0) ||
+              (xDisplacement == 0 && yDisplacement == 1)
+            ) {
+              const userSpace = await roomInstance.getSpaceData(this.spaceId);
+              if (!userSpace) {
+                this.ws.close();
+                return;
+              }
+              // should be within the space bounds
+              if (
+                x <= userSpace.width &&
+                x >= 0 &&
+                y >= 0 &&
+                y <= userSpace.height!
+              ) {
+                const spaceUsers = roomInstance.spaces.get(this.spaceId!);
+                const occupiedCoords = new Set<string>();
+                userSpace.occupiedStaticTiles.forEach((coord) =>
+                  occupiedCoords.add(coord)
+                );
+                spaceUsers?.forEach((u) => {
+                  if (u.id !== this.id) {
+                    occupiedCoords.add(`${u.x},${u.y}`);
+                  }
+                });
+                if (!occupiedCoords.has(`${x},${y}`)) {
+                  this.x = x;
+                  this.y = y;
+                  roomInstance.publish(
+                    {
+                      type: "movement",
+                      payload: {
+                        x: this.x,
+                        y: this.y,
+                        userId: this.userId!,
+                      },
+                    },
+                    this.id,
+                    this.spaceId!
+                  );
+                } else {
+                  this.ws.send(
+                    JSON.stringify({
+                      type: "movement-rejected",
+                      payload: {
+                        x: this.x,
+                        y: this.y,
+                      },
+                    })
+                  );
+                }
+              }
+            } else {
+              // rejected case, can only take one step either of the axis
+              this.ws.send(
+                JSON.stringify({
+                  type: "movement-rejected",
+                  payload: {
+                    x: this.x,
+                    y: this.y,
+                  },
+                })
+              );
+            }
+            break;
+          default:
+            break;
+        }
+      } catch (err) {
+        console.error("Websocket error: ", err);
+        this.ws.close();
       }
     });
   }
